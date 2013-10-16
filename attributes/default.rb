7bbb2e2bf5811d87d83f531d0c4806c8738e4497
Michael Hayes <tasslehawf@mac.com>
2013-10-16 16:53:22 -0500

    try maven 3.0.5

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
-node.override['maven']['3']['version'] = '3.1.1'
-node.override['maven']['3']['url'] = 'http://apache.mirrors.tds.net/maven/maven-3/3.1.1/binaries/apache-maven-3.1.1-bin.tar.gz'
+node.override['maven']['3']['version'] = '3.0.5'
+node.override['maven']['3']['url'] = 'http://apache.mirrors.tds.net/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz'
