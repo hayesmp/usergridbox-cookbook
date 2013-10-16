8fb58aba9ccf04f4edfa01382726ee428e308f71
Michael Hayes <tasslehawf@mac.com>
2013-10-16 16:15:50 -0500

    more proper chef commands

--- recipes/usergrid.rb
+++ recipes/usergrid.rb
@@ -3,10 +3,20 @@
 # Recipe:: usergrid
 #
 
-git "clone usergrid-stack" do
+git "#{Chef::Config[:file_cache_path]}" do
   repository "git://github.com:apigee/usergrid-stack.git"
   reference "master"
-  action :clone
+  action :checkout
+end
+
+bash "build usergrid" do
+  cwd "#{Chef::Config[:file_cache_path]}/usergrid-stack"
+  #user "rbenv"
+  #group "rbenv"
+  code <<-EOH
+     mvn clean install -DskipTests=true
+  EOH
+  environment 'PREFIX' => "/usr/local"
 end
 
 
@@ -14,10 +24,10 @@ end
 #  command "git clone git@github.com:apigee/usergrid-stack.git"
 #end
 
-execute "build usergrid-stack" do
-  command "cd usergrid-stack; mvn clean install -DskipTests=true"
-end
+#execute "build usergrid-stack" do
+#  command "cd usergrid-stack; mvn clean install -DskipTests=true"
+#end
 
 execute "start usergrid" do
-  command "cd usergrid-stack/launcher; java -jar target/usergrid-launcher-*.jar"
+  command "cd #{Chef::Config[:file_cache_path]}/usergrid-stack/launcher; java -jar target/usergrid-launcher-*.jar"
 end
