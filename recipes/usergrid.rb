ac9f5eae14341696f64ec6bda3123ac9dc0e41c8
Michael Hayes <tasslehawf@mac.com>
2013-10-16 16:26:53 -0500

    no, try this

--- recipes/usergrid.rb
+++ recipes/usergrid.rb
@@ -9,25 +9,27 @@ git "#{Chef::Config[:file_cache_path]}" do
   action :checkout
 end
 
-bash "build usergrid" do
-  cwd "#{Chef::Config[:file_cache_path]}/usergrid-stack"
-  #user "rbenv"
-  #group "rbenv"
-  code <<-EOH
-     mvn clean install -DskipTests=true
-  EOH
-  environment 'PREFIX' => "/usr/local"
-end
+#bash "build usergrid" do
+#  cwd "#{Chef::Config[:file_cache_path]}/usergrid-stack"
+#  #user "rbenv"
+#  #group "rbenv"
+#  code <<-EOH
+#     mvn clean install -DskipTests=true
+#  EOH
+#  environment 'PREFIX' => "/usr/local"
+#end
 
 
 #execute "clone usergrid-stack" do
 #  command "git clone git@github.com:apigee/usergrid-stack.git"
 #end
 
-#execute "build usergrid-stack" do
-#  command "cd usergrid-stack; mvn clean install -DskipTests=true"
-#end
+execute "build usergrid-stack" do
+  cwd "#{Chef::Config[:file_cache_path]}/usergrid-stack"
+  command "mvn clean install -DskipTests=true"
+end
 
 execute "start usergrid" do
-  command "cd #{Chef::Config[:file_cache_path]}/usergrid-stack/launcher; java -jar target/usergrid-launcher-*.jar"
+  cwd "#{Chef::Config[:file_cache_path]}/usergrid-stack/launcher"
+  command "java -jar target/usergrid-launcher-*.jar"
 end
