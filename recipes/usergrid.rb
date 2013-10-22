5a1440ea6e47b7968a08f7d064ed469b70b9fdff
Michael Hayes <tasslehawf@mac.com>
2013-10-22 10:04:58 -0500

    nates repo doesn't work anyway

--- recipes/usergrid.rb
+++ recipes/usergrid.rb
@@ -14,19 +14,19 @@ execute "build usergrid-stack" do
   command "mvn clean install -DskipTests=true -e"
 end
 
-git "#{Chef::Config[:file_cache_path]}/usergrid-rest-apigee-sample" do
-  repository "git://github.com/zznate/usergrid-rest-apigee-sample.git"
-  reference "master"
-  action :checkout
-end
-
-execute "build usergrid-rest-apigee-sample" do
-  cwd "#{Chef::Config[:file_cache_path]}/usergrid-rest-apigee-sample"
-  command "mvn install"
-end
-
-
-execute "start usergrid" do
-  cwd "#{Chef::Config[:file_cache_path]}/usergrid/launcher"
-  command "java -jar target/usergrid-launcher-*.jar"
-end
+#git "#{Chef::Config[:file_cache_path]}/usergrid-rest-apigee-sample" do
+#  repository "git://github.com/zznate/usergrid-rest-apigee-sample.git"
+#  reference "master"
+#  action :checkout
+#end
+#
+#execute "build usergrid-rest-apigee-sample" do
+#  cwd "#{Chef::Config[:file_cache_path]}/usergrid-rest-apigee-sample"
+#  command "mvn install"
+#end
+#
+#
+#execute "start usergrid" do
+#  cwd "#{Chef::Config[:file_cache_path]}/usergrid/launcher"
+#  command "java -jar target/usergrid-launcher-*.jar"
+#end
