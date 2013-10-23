5c01a770fd593708001f7fe9ceb496b612b10339
Michael Hayes <tasslehawf@mac.com>
2013-10-23 12:23:18 -0500

    delete default root in tomcat and copy usergrid war

--- recipes/usergrid.rb
+++ recipes/usergrid.rb
@@ -14,19 +14,28 @@ execute "build usergrid-stack" do
   command "mvn clean install -DskipTests=true -e"
 end
 
-#git "#{Chef::Config[:file_cache_path]}/usergrid-rest-apigee-sample" do
-#  repository "git://github.com/zznate/usergrid-rest-apigee-sample.git"
-#  reference "master"
-#  action :checkout
-#end
-#
-#execute "build usergrid-rest-apigee-sample" do
-#  cwd "#{Chef::Config[:file_cache_path]}/usergrid-rest-apigee-sample"
-#  command "mvn install"
-#end
-#
-#
-#execute "start usergrid" do
-#  cwd "#{Chef::Config[:file_cache_path]}/usergrid/launcher"
-#  command "java -jar target/usergrid-launcher-*.jar"
-#end
+#rm -rf /var/lib/tomcat6/webapps/ROOT
+file "/var/lib/tomcat6/webapps/ROOT" do
+  owner "root"
+  group "root"
+  #mode "0755"
+  action :delete
+end
+
+#cp /var/chef/cache/usergrid/rest/target/ROOT.war /var/lib/tomcat6/webapps/
+execute "copy usergrid war into tomcat" do
+  command "cp /var/chef/cache/usergrid/rest/target/ROOT.war /var/lib/tomcat6/webapps/"
+end
+
+#Edit /var/chef/cache/usergrid/config/src/main/resources/usergrid-default.properties
+  #cassandra.url=localhost:9160
+  #cassandra.username=
+  #cassandra.password=
+
+  ## SysAdmin login
+  #usergrid.sysadmin.login.name=
+  #usergrid.sysadmin.login.email=
+  #usergrid.sysadmin.login.password=
+  #usergrid.sysadmin.login.allowed=false
+
+  #Go to <ip address>:8080/setup
