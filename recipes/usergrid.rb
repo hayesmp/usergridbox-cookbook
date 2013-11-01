7f183870646d27a0cf868d5eda44e0426b5540f2
Michael Hayes <tasslehawf@mac.com>
2013-11-01 10:40:52 -0500

    run the usergrid build in debug mode; try to initialize the database right after setup

--- recipes/usergrid.rb
+++ recipes/usergrid.rb
@@ -32,7 +32,7 @@ end
 
 execute "build usergrid-stack" do
   cwd "#{Chef::Config[:file_cache_path]}/usergrid"
-  command "mvn clean install -DskipTests=true -e"
+  command "mvn clean install -DskipTests=true -e -X"
 end
 
 #rm -rf /var/lib/tomcat6/webapps/ROOT
@@ -45,15 +45,6 @@ execute "copy usergrid war into tomcat" do
   command "cp /var/chef/cache/usergrid/rest/target/ROOT.war /var/lib/tomcat6/webapps/"
 end
 
-#vi /var/chef/cache/usergrid/config/src/main/resources/usergrid-default.properties
-#cassandra.url=localhost:9160
-#cassandra.username=
-#cassandra.password=
-
-## SysAdmin login
-#usergrid.sysadmin.login.name=
-#usergrid.sysadmin.login.email=
-#usergrid.sysadmin.login.password=
-#usergrid.sysadmin.login.allowed=false
-
-#Go to <ip address>:8080/setup
+execute "initialize database" do
+  command "curl -v --user admin:admin_pass http://localhost:8080/system/database/setup"
+end
